e3330839954ceb03ff9ddfb4ee64ec8e851b1d22
DavidMwangi <david.mathenge98@gmail.com>
2017-10-24 17:47:08 +0300

    Tests on CSV Dict

--- MTPythonSampleCode/testscsv.py
+++ MTPythonSampleCode/testscsv.py
@@ -1,10 +1,41 @@
 import csv
 
-with open('translate.csv', 'r') as f:
+"""with open('test.csv', 'r') as f:
 
-    r = csv.reader(f, delimiter=',')
+    
 
-    for row in r:
+    reader = csv.DictReader(f,delimiter=',')
 
+    for row in reader:
 
-        print row[2]
+        row['Title'] = 'Hello'
+
+        row['ID'] = 1
+
+        row['ru'] ='Russian'
+
+        row['sp'] = 'Spanish'
+
+        row['du'] = 'Dutch'"""
+
+with open('out.csv', 'a') as l:
+
+    fieldnames = ['Title','ID','ru','sp','du']
+    
+    writer = csv.DictWriter(l, fieldnames=fieldnames)
+
+    writer.writeheader()
+
+    with open('test.csv','r') as f:
+
+        reader = csv.reader(f, delimiter=',')
+
+        for row in reader:
+
+            writer.writerow({'Title': row[0],'ID':row[1], 'ru':row[2],'sp':row[3],'du':row[4]})
+    
+    """
+    for row in reader:
+
+        writer.writerow(row)
+    """
