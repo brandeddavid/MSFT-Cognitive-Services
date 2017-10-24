59900170a34a181c6b999ff46ffde93c68c263ad
DavidMwangi <david.mathenge98@gmail.com>
2017-10-24 18:11:24 +0300

    Before Optimization

--- MTPythonSampleCode/clonedict.py
+++ MTPythonSampleCode/clonedict.py
@@ -18,10 +18,10 @@ sys.setdefaultencoding('utf-8')
 def GetTextAndTranslate(finalToken):
 
     fromLangCode = "en"
-    
 
 
-    with open('out.csv', 'a',newline='') as l:
+
+    with open('out.csv', 'a') as l:
 
         fieldnames = ['Title','ID','ru','sp','du']
 
@@ -35,26 +35,25 @@ def GetTextAndTranslate(finalToken):
 
             for item in reader:
 
-                textToTranslate = item[0]                
                 headers = {"Authorization ": finalToken}
 
                 #Translate to Russia
-                translateRUUrl = "http://api.microsofttranslator.com/v2/Http.svc/Translate?text={}&to={}".format(textToTranslate, 'ru')
+                translateRUUrl = "http://api.microsofttranslator.com/v2/Http.svc/Translate?text={}&to={}".format(item[0], 'ru')
                 translationRUData = requests.get(translateRUUrl, headers = headers)
-                translationRU = ElementTree.fromstring(translationDataRU.text.encode('utf-8'))
-                row [2] = translationRU.text
+                translationRU = ElementTree.fromstring(translationRUData.text.encode('utf-8'))
+                row[2] = translationRU.text
 
                 #Translate to Spanish
-                translateESUrl = "http://api.microsofttranslator.com/v2/Http.svc/Translate?text={}&to={}".format(textToTranslate, 'es')
+                translateESUrl = "http://api.microsofttranslator.com/v2/Http.svc/Translate?text={}&to={}".format(item[0], 'es')
                 translationESData = requests.get(translateESUrl, headers = headers)
                 translationES = ElementTree.fromstring(translationESData.text.encode('utf-8'))
-                row [3] = translationES.text
+                row[3] = translationES.text
 
                 #Translate to Dutch
-                translateNLUrl = "http://api.microsofttranslator.com/v2/Http.svc/Translate?text={}&to={}".format(textToTranslate, 'nl')
+                translateNLUrl = "http://api.microsofttranslator.com/v2/Http.svc/Translate?text={}&to={}".format(item[0], 'nl')
                 translationNLData = requests.get(translateNLUrl, headers = headers)
                 translationNL = ElementTree.fromstring(translationNLData.text.encode('utf-8'))
-                row [4] = translationNL.text
+                row[4] = translationNL.text
 
                 writer.writerow({'Title': item[0],'ID':item[1], 'ru':item[2],'sp':item[3],'du':[4]})
 
