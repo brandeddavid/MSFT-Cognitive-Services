423f05fe3e8e68f902e65cab9212028bcd56c621
brandeddavid <david.mathenge98@gmail.com>
2017-12-05 11:28:13 +0300

    Add init text before/after run

--- Bing Search API/parser.py
+++ Bing Search API/parser.py
@@ -1,10 +1,10 @@
 import requests, re, csv
-
+from urllib import request
 from bs4 import BeautifulSoup
 
 with open('final.csv', 'a', newline = '') as l:
 
-    fieldnames = ['name', 'url','emails', 'phone-numbers','search-condition', 'http status']
+    fieldnames = ['name', 'url','emails', 'phone-numbers','search-condition', 'http status', 'text before', 'text after']
 
     writer = csv.DictWriter(l, fieldnames=fieldnames)
 
@@ -22,6 +22,7 @@ with open('final.csv', 'a', newline = '') as l:
 
             emails = re.findall(r'mailto\:[0-9a-zA-Z\@\.]{1,}', r.text)
 
+
             #phones = re.findall(r'[(][\d]{3}[)][ ]?[\d]{3}-[\d]{4}', r.text)
 
             if len(re.findall(r'[(][\d]{3}[)][ ]?[\d]{3}-[\d]{4}', r.text)) == 0:
@@ -32,6 +33,17 @@ with open('final.csv', 'a', newline = '') as l:
 
                 phones = re.findall(r'[(][\d]{3}[)][ ]?[\d]{3}-[\d]{4}', r.text)
 
+            data = request.urlopen(item[1]).read().decode('utf-8')
+            #re.search(r'mailto\:[0-9a-zA-Z\@\.]{1,}', r.text)
+
+            details = re.search('contact', data)
+
+            end = details.end()
+            textAfter = data[end:end+100]
+
+            start = details.start()
+            textBefore = data[start-100:start]
+
             #[(][\d]{3}[)][ ]?[\d]{3}-[\d]{4} Best so far
             #(\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]??\d{4}|\d{3}[-\.\s]??\d{4}) Works just fine
             #\:[\(\)\-0-9\ ]{1,} Works jumbled up
@@ -39,4 +51,4 @@ with open('final.csv', 'a', newline = '') as l:
             #(9\d)\s+(\d{2})\s+(\d{2})\s+(\d{3}) Works no results
             #(1[-.])*([2-9]\d{2})?[-. ]\d{3}[-. ]\d{4} Works a bit
 
-            writer.writerow({'name': item[0],'url':item[1], 'emails':[email[7:] for email in emails], 'phone-numbers':[phone for phone in phones], 'search-condition':'intitle:Contact (ELL OR ESL)  site:.us ("public school" OR "public schools")', 'http status':r.status_code})
+            writer.writerow({'name': item[0],'url':item[1], 'emails':[email[7:] for email in emails], 'phone-numbers':[phone for phone in phones], 'search-condition':'intitle:Contact (ELL OR ESL)  site:.us ("public school" OR "public schools")', 'http status':r.status_code, 'text before':textBefore, 'text after':textAfter})
