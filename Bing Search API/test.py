9cc245e7b4510e0d7dfcec16ad5bde2434260b38
DavidMwangi <david.mathenge98@gmail.com>
2018-01-22 16:32:13 +0300

    Add India Search Test

---
+++ Bing Search API/test.py
@@ -0,0 +1,57 @@
+import http.client, urllib.parse, json, csv
+
+subscriptionKey = "763b746a12c74bcca27a50d1a48304a0"
+
+host = "api.cognitive.microsoft.com"
+path = "/bing/v7.0/search"
+
+
+def BingWebSearch(search):
+    "Performs a Bing Web search and returns the results."
+
+    addquery = '&count=50&offset=0&cc=IN'
+
+    headers = {'Ocp-Apim-Subscription-Key': subscriptionKey}
+    conn = http.client.HTTPSConnection(host)
+    query = urllib.parse.quote(search)
+    conn.request("GET", path + "?q=" + query + addquery, headers=headers)
+    response = conn.getresponse()
+    headers = [k + ": " + v for (k, v) in response.getheaders()
+               if k.startswith("BingAPIs-") or k.startswith("X-MSEdge-")]
+    return headers, response.read().decode("utf8")
+
+
+with open('urls.csv', 'a', newline='') as l:
+
+    fieldnames = ['NAME', 'URL', 'TERM']
+
+    writer = csv.DictWriter(l, fieldnames=fieldnames)
+    writer.writeheader()
+
+    searchphrase = 'school "english language" intitle:contact site:.in'
+
+    if len(subscriptionKey) == 32:
+
+        try:
+
+            print('Searching the Web for: ', searchphrase)
+
+            headers, result = BingWebSearch(searchphrase)
+
+            jsonResponse = json.loads(json.dumps(json.loads(result), indent=4))
+
+            for item in jsonResponse['webPages']['value']:
+
+                if jsonResponse['webPages']['value'].index(item) < 50:
+                    writer.writerow({'NAME': item['name'], 'URL': item['url'], 'TERM': searchphrase})
+
+        except:
+
+            pass
+
+
+    else:
+
+        print("Invalid Bing Search API subscription key!")
+        print("Please paste yours into the source code.")
+
